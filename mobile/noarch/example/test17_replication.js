9fc0284a39e7b184e464534a962e2dab20f91715
Paul Mietz Egli <paul@obscure.com>
2012-08-28 14:21:23 -0700

    added test for push replication

--- mobile/noarch/example/test17_replication.js
+++ mobile/noarch/example/test17_replication.js
@@ -5,22 +5,37 @@ var _ = require('underscore'),
 
 exports.run_tests = function() {
     var db = server.databaseNamed('test17');
-    db.deleteDatabase();
     db.ensureCreated();
 
-    var replicationComplete = false, checkCount = 15;
+    var pullComplete = false, pushComplete = false, checkCount = 15;
 
     try {
+      var dt = new Date();
+      createDocWithProperties(db, {
+        testname: 'push_to_remote',
+        timestamp: dt.getTime() / 1000,
+      }, 'zzz');
+      
       // start with one replication at the beginning
-      var repl = db.pullFromDatabaseAtURL('http://touchbooks.iriscouch.com/books');
-      repl.addEventListener('progress', function(e) {
-        Ti.API.info('progress: '+JSON.stringify(e));
+      var pull = db.pullFromDatabaseAtURL('http://touchbooks.iriscouch.com/books');
+      pull.addEventListener('progress', function(e) {
+        Ti.API.info('pull progress: '+JSON.stringify(e));
       });
-      repl.addEventListener('stopped', function(e) {
-        Ti.API.info('stopped: '+JSON.stringify(e));
-        replicationComplete = (e.completed >= e.total);
+      pull.addEventListener('stopped', function(e) {
+        Ti.API.info('pull stopped: '+JSON.stringify(e));
+        pullComplete = (e.completed >= e.total);
       });
-      repl.start();
+      pull.start();
+      
+      var push = db.pushToDatabaseAtURL('http://touchbooks.iriscouch.com/test');
+      push.addEventListener('progress', function(e) {
+        Ti.API.info('push progress: '+JSON.stringify(e));
+      });
+      push.addEventListener('stopped', function(e) {
+        Ti.API.info('push stopped: '+JSON.stringify(e));
+        pushComplete = (e.completed >= e.total);
+      });
+      push.start();
     }
     catch (e) {
         db.deleteDatabase();
@@ -28,8 +43,8 @@ exports.run_tests = function() {
     }
 
     var interval = setInterval(function() {
-      if (replicationComplete) {
-        Ti.API.info("replication done!");
+      if (pullComplete && pushComplete) {
+        Ti.API.info("replication done!  doc count = "+db.getDocumentCount());
         clearInterval(interval);
         db.deleteDatabase();
       }
