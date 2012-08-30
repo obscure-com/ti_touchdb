4cfde6eee6c982a22e2d9225bce33aadf23c00e0
Paul Mietz Egli <paul@obscure.com>
2012-08-30 14:46:27 -0700

    removed old TouchBooks sample

--- samples/TouchBooks.old/Resources/app.js
+++
@@ -1,2 +0,0 @@
-var app = require('/ui/bootstrap');
-app.launch();
--- samples/TouchBooks.old/Resources/iphone/ui/BasicWindow.js
+++
@@ -1,8 +0,0 @@
-
-function BasicWindow() {
-  var result = Ti.UI.createWindow();
-  // TODO customize window as needed
-  return result;
-}
-
-module.exports = BasicWindow;
