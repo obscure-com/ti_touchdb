07d68d9886247211d0cdb92426e22df0c5c5aa88
Paul Mietz Egli <paul@obscure.com>
2012-10-08 13:34:32 -0700

    added document loader code and sample docs

--- samples/TouchBooksAlloy/app/alloy.js
+++ samples/TouchBooksAlloy/app/alloy.js
@@ -7,5 +7,42 @@ var server = require('com.obscure.titouchdb'),
 
 db.ensureCreated();
 
-// TODO load schema
+// load up JSON documents in the "schema" directory
+var schemadoc = db.documentWithID('schema');
+var applied = schemadoc ? schemadoc.properties.applied || [] : [];
 
+var schemadir = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory, 'schema');
+if (schemadir.exists()) {
+  var dirty = false;
+  
+  var fns = schemadir.getDirectoryListing();
+  _.each(fns, function(fn) {
+    var f = Ti.Filesystem.getFile(schemadir.resolve(), fn);
+    var contents = f.exists() ? f.read().text : null;
+    var hash = contents ? Ti.Utils.md5HexDigest(contents) : null;
+    if (hash && applied.indexOf(hash) === -1) {        
+      var json = JSON.parse(contents);
+      // get the existing revision if the doc already exists
+      _.each(json.docs, function(doc) {
+        if (doc._id) {
+          var existing = db.documentWithID(doc._id);
+          if (existing.currentRevisionID) {
+            doc._rev = existing.currentRevisionID;
+          }
+        }
+      });
+      db.putChanges(json.docs);
+      applied.push(hash);
+      dirty = true;
+    }
+  });
+  
+  if (dirty) {
+    schemadoc.putProperties({
+      applied: applied
+    });
+  }
+} 
+else {
+  Ti.API.error('Schema directory ' + schemadir.resolve() + ' does not exist');
+}
