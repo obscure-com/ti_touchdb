84f28ce337ac1a9db7a856dfe145830775eb604b
Paul Mietz Egli <paul@obscure.com>
2012-10-09 15:04:45 -0700

    added grouping code.

--- samples/TouchBooksAlloy/app/controllers/book_list.js
+++ samples/TouchBooksAlloy/app/controllers/book_list.js
@@ -1,4 +1,6 @@
 
+var currentView = 'by_author';
+
 var books = Alloy.createCollection("Book");
 books.on('fetch', function(e) {
   $.tableView.refresh(books);
@@ -6,26 +8,63 @@ books.on('fetch', function(e) {
 
 $.tableView.refresh = function(collection) {
   var data = [];
+  var section = null;
   collection.each(function(book) {
+    var groupProperty;
+    if (currentView === 'by_author') {
+      groupProperty = book.get('author');
+    }
+    else if (currentView === 'by_published') {
+      groupProperty = book.get('published')[0];
+    }
+    
+    if (!section || section.headerTitle !== groupProperty) {
+      if (section) {
+        data.push(section);
+      }
+      section = Ti.UI.createTableViewSection({
+        headerTitle: groupProperty
+      });
+    }
+    
     var row = Ti.UI.createTableViewRow({
       title: book.get('title')
     });
-    data.push(row);
+    section.add(row);
   });
+  data.push(section); // last section
+  
   this.setData(data);
 };
 
-function loadBookList(e) {
-  books.fetch();
+function loadBookList() {
+  books.fetch({ view: currentView });
 }
 
 function changeGrouping(e) {
   if (OS_ANDROID) {
     if (e.source.id === 'authorSwitch') {
+      if (e.source.value) {
+        currentView = 'by_author';
+      }
       $.publishedSwitch.value = !e.source.value;
     }
     else if (e.source.id === 'publishedSwitch') {
+      if (e.source.value) {
+        currentView = 'by_published';
+      }
       $.authorSwitch.value = !e.source.value;
     }
   }
+  else if (OS_IOS) {
+    switch (e.index) {
+      case 0:
+        currentView = 'by_author';
+        break;
+      case 1:
+        currentView = 'by_published';
+        break;
+    }
+  }
+  loadBookList();
 }
