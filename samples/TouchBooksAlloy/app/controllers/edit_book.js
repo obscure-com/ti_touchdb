e7f2c6e2645c0b6242415c2b8d66e1210180ed1d
Paul Mietz Egli <paul@obscure.com>
2012-10-15 10:19:51 -0700

    fixed publication date. The Date constructor that takes year, month, day, etc. creates the date object in GMT, which was causing trouble with the date picker UI widgets.

--- samples/TouchBooksAlloy/app/controllers/edit_book.js
+++ samples/TouchBooksAlloy/app/controllers/edit_book.js
@@ -1,19 +1,35 @@
 
 var book = Alloy.createModel('Book');
+
 book.on('fetch', function(obj) {
   $.isbn.text = book.id;
   $.title.value = book.get('title');
   $.author.value = book.get('author');
 
-  var published = book.get('published') || [];
-  var publishedDate = new Date(
-    published.length > 0 ? published[0] : 1970,
-    (published.length > 1 ? published[1] : 1) - 1,
-    published.length > 2 ? published[2] : 1);
+  var published = book.get('published') || [1970];
+  var publishedDate = new Date();
+  publishedDate.setFullYear(published.length > 0 ? published[0] : 1970);
+  publishedDate.setMonth(published.length > 1 ? published[1] - 1 : 0);
+  publishedDate.setDate(published.length > 2 ? published[2] : 1);
   $.published.value = publishedDate;
+});
+
+book.on('update', function() {
+  Ti.App.fireEvent('books:book_changed', { book_id: book.id });
+});
+
+book.on('create', function() {
+  Ti.App.fireEvent('books:book_changed', { book_id: book.id });
 });
 
 exports.set_book_id = function(id) {
   book.id = id;
   book.fetch();
 };
+
+function saveChanges(e) {
+  book.set('title', $.title.value);
+  book.set('author', $.author.value);
+  book.set('published', [$.published.value.getFullYear(), $.published.value.getMonth() + 1, $.published.value.getDate()]);
+  book.save();
+}
