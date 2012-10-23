6342575e93729737029c0e558a934ba46f89c670
Paul Mietz Egli <paul@obscure.com>
2012-10-23 11:55:42 -0700

    set the table view row image in the controller

--- samples/TouchBooksAlloy/app/controllers/book_list_row.js
+++ samples/TouchBooksAlloy/app/controllers/book_list_row.js
@@ -1,7 +1,9 @@
 var book = arguments && arguments[0] || {};
 
 $.title.text = book.get('title');
+
 // TODO book attachment to cover view
+$.cover.image = "/images/text_page.png";
 
 $.tableViewRow.addEventListener('click', function(e) {
   Ti.App.fireEvent('books:edit_book', {
