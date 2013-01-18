61a364a16f51b30ac19801681df721b4b75ee4dc
Paul Mietz Egli <paul@obscure.com>
2013-01-18 09:47:38 -0800

    added view unit test file

---
+++ mobile/noarch/example/009_views.js
@@ -0,0 +1,76 @@
+Ti.include('test_utils.js')
+
+var _ = require('underscore'),
+    touchdb = require('com.obscure.titouchdb');
+
+exports.run_tests = function() {
+  var mgr = touchdb.databaseManager;
+  var db = mgr.createDatabaseNamed('test009');
+  
+  try {
+    var view = db.viewNamed('vu');
+    assert(view, 'db.viewNamed() returned null');
+    assert(view.name === 'vu', 'returned incorrect view name: '+view.name);
+    
+    view.setMap(function(doc, emit) {
+      emit(doc.name, doc.i);
+    }, '1');
+
+    for (i=0; i < 50; i++) {
+      createDocWithProperties(db, {
+        name: String.format('test%02d', i),
+        i: i
+      });
+    }
+    
+    (function() {
+      var rows = view.query().rows();
+      assert(rows, 'no rows for view query');
+      assert(rows.count == 50, 'incorrect number of rows: '+rows.count);
+
+      for (i=0; i < 50; i++) {
+        var row = rows.rowAtIndex(i);
+        assert(row.key === String.format('test%02d', i), 'incorrect row key: '+row.key+", should be "+String.format('test%02d', i));
+        assert(row.value === i, 'incorrect row value: '+row.value);
+      }
+    })();
+    
+    (function() {
+      // test query parameters
+      var query = view.query();
+      query.limit = 10;
+      query.skip = 10;
+      var rows = query.rows();
+      assert(rows.count === 10, 'incorrect number of rows returned (limit): '+rows.count);
+      for (var i=0; i < 10; i++) {
+        var row = rows.rowAtIndex(i);
+        assert(row.key === String.format('test%02d', i+query.skip), 'incorrect row key: '+row.key+", should be "+String.format('test%02d', i+query.skip));
+        assert(row.value === i+query.skip, 'incorrect row value: '+row.value);
+      }
+    })();
+    
+    (function(){
+      var query = view.query();
+      query.startKey = 'test22';
+      query.endKey = 'test28';
+      var rows = query.rows();
+      assert(rows.count === 7, 'incorrect number of rows returned (startkey): '+rows.count);
+      for (var i=0; i < 7; i++) {
+        var row = rows.rowAtIndex(i);
+        assert(row.key === String.format('test%02d', i+22), 'incorrect row key: '+row.key+", should be "+String.format('test%02d', i+22));
+        assert(row.value === i+22, 'incorrect row value: '+row.value);
+      }
+      
+    })();
+    
+    (function(){})();
+    (function(){})();
+    
+    db.deleteDatabase();
+  }
+  catch (e) {
+    db.deleteDatabase();
+    throw e;
+  }
+
+}  
