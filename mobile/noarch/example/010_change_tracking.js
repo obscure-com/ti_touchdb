54bebb65a5cc25403148b8a6359245e47098f1c3
Paul Mietz Egli <paul@obscure.com>
2013-01-25 16:32:41 -0800

    updated change tracking test

--- mobile/noarch/example/010_change_tracking.js
+++ mobile/noarch/example/010_change_tracking.js
@@ -7,28 +7,21 @@ exports.run_tests = function() {
   var mgr = touchdb.databaseManager;
   var db = mgr.createDatabaseNamed('test010');
   
-  db.addEventListener('test', function(e) {
-    alert("fired test");
-  });
-
-
   try {
     var changes = 0;
-    db.addEventListener('gnee', function(e) {
-      alert("gnee!");
+
+    db.addEventListener('change', function(e) {
       Ti.API.info(JSON.stringify(e));
       ++changes;
     });
-    
-    db.eventTest();
 
-    /*
     createDocuments(db, 3);
     
     wait(2000);
-    assert(changes > 0, 'no changes detected');
     
-    */
+    // database change events not currently working
+    // assert(changes > 0, 'no changes detected');
+
     db.deleteDatabase();
   }
   catch (e) {
