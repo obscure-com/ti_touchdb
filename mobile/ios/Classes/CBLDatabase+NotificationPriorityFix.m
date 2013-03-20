5eba5c300ef2f4740f80ecd2f3a645902e744776
Paul Mietz Egli <paul@obscure.com>
2013-03-20 10:47:54 -0700

    updated to latest replication change method

--- mobile/ios/Classes/CBLDatabase+NotificationPriorityFix.m
+++ mobile/ios/Classes/CBLDatabase+NotificationPriorityFix.m
@@ -14,32 +14,28 @@
 
 extern NSString* const CBL_DatabaseChangesNotification;
 
-- (void) tddbNotification: (NSNotification*)n {
-    if ([n.name isEqualToString: CBL_DatabaseChangesNotification]) {
-        for (CBL_DatabaseChange* change in (n.userInfo)[@"changes"]) {
-            CBL_Revision* winningRev = change.winningRevision;
-            NSURL* source = change.source;
-            
-            // Notify the corresponding instantiated CBLDocument object (if any):
-            [[self cachedDocumentWithID: winningRev.docID] revisionAdded: change];
-            
-            // Post a database-changed notification, but only post one per runloop cycle by using
-            // a notification queue. If the current notification has the "external" flag, make sure
-            // it gets posted by clearing any pending instance of the notification that doesn't have
-            // the flag.
-            NSDictionary* userInfo = source ? $dict({@"external", $true}) : nil;
-            NSNotification* n = [NSNotification notificationWithName: kCBLDatabaseChangeNotification
-                                                              object: self
-                                                            userInfo: userInfo];
-            NSNotificationQueue* queue = [NSNotificationQueue defaultQueue];
-            if (source != nil)
-                [queue dequeueNotificationsMatching: n coalesceMask: NSNotificationCoalescingOnSender];
-            [queue enqueueNotification: n
-                          postingStyle: NSPostNow
-                          coalesceMask: NSNotificationCoalescingOnSender
-                              forModes: @[NSRunLoopCommonModes]];
-        }
-    }
+- (void) postPublicChangeNotification: (CBL_DatabaseChange*)change {
+    CBL_Revision* winningRev = change.winningRevision;
+    NSURL* source = change.source;
+    
+    // Notify the corresponding instantiated CBLDocument object (if any):
+    [[self cachedDocumentWithID: winningRev.docID] revisionAdded: change];
+    
+    // Post a database-changed notification, but only post one per runloop cycle by using
+    // a notification queue. If the current notification has the "external" flag, make sure
+    // it gets posted by clearing any pending instance of the notification that doesn't have
+    // the flag.
+    NSDictionary* userInfo = source ? $dict({@"external", $true}) : nil;
+    NSNotification* n = [NSNotification notificationWithName: kCBLDatabaseChangeNotification
+                                                      object: self
+                                                    userInfo: userInfo];
+    NSNotificationQueue* queue = [NSNotificationQueue defaultQueue];
+    if (source != nil)
+        [queue dequeueNotificationsMatching: n coalesceMask: NSNotificationCoalescingOnSender];
+    [queue enqueueNotification: n
+                  postingStyle: NSPostNow
+                  coalesceMask: NSNotificationCoalescingOnSender
+                      forModes: @[NSRunLoopCommonModes]];
 }
 
 @end
