59d67e349900f8fb094eb82fd454918c91f4097b
Paul Mietz Egli <paul@obscure.com>
2013-04-03 16:34:17 -0700

    added call to compact() in the database creation function

--- samples/CannedDatabase/Resources/app.js
+++ samples/CannedDatabase/Resources/app.js
@@ -15,6 +15,7 @@ function generateDB() {
   var elements = [[1, "Hydrogen", "H"], [2, "Helium", "He"], [3, "Lithium", "Li"], [4, "Beryllium", "Be"], [5, "Boron", "B"], [6, "Carbon", "C"], [7, "Nitrogen", "N"], [8, "Oxygen", "O"], [9, "Fluorine", "F"], [10, "Neon", "Ne"], [11, "Sodium", "Na"], [12, "Magnesium", "Mg"], [13, "Aluminium", "Al", "http://0.tqn.com/d/chemistry/1/6/q/V/1/Aluminium.jpg"], [14, "Silicon", "Si"], [15, "Phosphorus", "P"], [16, "Sulfur", "S"], [17, "Chlorine", "Cl"], [18, "Argon", "Ar"], [19, "Potassium", "K"], [20, "Calcium", "Ca"], [21, "Scandium", "Sc"], [22, "Titanium", "Ti"], [23, "Vanadium", "V"], [24, "Chromium", "Cr"], [25, "Manganese", "Mn"], [26, "Iron", "Fe"], [27, "Cobalt", "Co", "http://0.tqn.com/d/chemistry/1/6/I/Z/1/cobalt.jpg"], [28, "Nickel", "Ni"], [29, "Copper", "Cu"], [30, "Zinc", "Zn"], [31, "Gallium", "Ga", "http://0.tqn.com/d/chemistry/1/6/H/Q/gallium.jpg"], [32, "Germanium", "Ge"], [33, "Arsenic", "As"], [34, "Selenium", "Se"], [35, "Bromine", "Br"], [36, "Krypton", "Kr"], [37, "Rubidium", "Rb"], [38, "Strontium", "Sr"], [39, "Yttrium", "Y"], [40, "Zirconium", "Zr"], [41, "Niobium", "Nb"], [42, "Molybdenum", "Mo"], [43, "Technetium", "Tc"], [44, "Ruthenium", "Ru"], [45, "Rhodium", "Rh"], [46, "Palladium", "Pd"], [47, "Silver", "Ag"], [48, "Cadmium", "Cd"], [49, "Indium", "In"], [50, "Tin", "Sn"], [51, "Antimony", "Sb"], [52, "Tellurium", "Te"], [53, "Iodine", "I"], [54, "Xenon", "Xe"], [55, "Caesium", "Cs"], [56, "Barium", "Ba"], [71, "Lutetium", "Lu"], [72, "Hafnium", "Hf"], [73, "Tantalum", "Ta"], [74, "Tungsten", "W"], [75, "Rhenium", "Re"], [76, "Osmium", "Os"], [77, "Iridium", "Ir"], [78, "Platinum", "Pt"], [79, "Gold", "Au"], [80, "Mercury", "Hg"], [81, "Thallium", "Tl"], [82, "Lead", "Pb"], [83, "Bismuth", "Bi", "http://0.tqn.com/d/chemistry/1/6/m/Q/bismuth.jpg"], [84, "Polonium", "Po"], [85, "Astatine", "At"], [86, "Radon", "Rn"], [87, "Francium", "Fr"], [88, "Radium", "Ra"], [103, "Lawrencium", "Lr"], [104, "Rutherfordium", "Rf"], [105, "Dubnium", "Db"], [106, "Seaborgium", "Sg"], [107, "Bohrium", "Bh"], [108, "Hassium", "Hs"], [109, "Meitnerium", "Mt"], [110, "Darmstadtium", "Ds"], [111, "Roentgenium", "Rg"], [112, "Copernicium", "Cn"], [113, "Ununtrium", "Uut"], [114, "Ununquadium", "Uuq"], [115, "Ununpentium", "Uup"], [116, "Ununhexium", "Uuh"], [117, "Ununseptium", "Uus"], [118, "Ununoctium", "Uuo"], [57, "Lanthanum", "La"], [58, "Cerium", "Ce"], [59, "Praseodymium", "Pr"], [60, "Neodymium", "Nd"], [61, "Promethium", "Pm"], [62, "Samarium", "Sm"], [63, "Europium", "Eu"], [64, "Gadolinium", "Gd"], [65, "Terbium", "Tb"], [66, "Dysprosium", "Dy"], [67, "Holmium", "Ho"], [68, "Erbium", "Er"], [69, "Thulium", "Tm"], [70, "Ytterbium", "Yb"], [89, "Actinium", "Ac"], [90, "Thorium", "Th"], [91, "Protactinium", "Pa"], [92, "Uranium", "U"], [93, "Neptunium", "Np"], [94, "Plutonium", "Pu"], [95, "Americium", "Am"], [96, "Curium", "Cm"], [97, "Berkelium", "Bk"], [98, "Californium", "Cf"], [99, "Einsteinium", "Es"], [100, "Fermium", "Fm"], [101, "Mendelevium", "Md"], [102, "Nobelium", "No"]];
   
   function saveAttachment(doc, url) {
+    ++attcount''
     var client = Ti.Network.createHTTPClient({
       onload: function(e) {
         var rev = doc.newRevision();
@@ -26,6 +27,7 @@ function generateDB() {
     client.send();
   }
   
+  var attcount = 0;
   for (i=0; i < elements.length; i++) {
     var e = elements[i];
     var doc = db.untitledDocument();
@@ -39,6 +41,12 @@ function generateDB() {
       saveAttachment(doc, e[3]);
     }
   }
+  
+  /*
+   * When creating a database for loading, you MUST run compact() after inserting data
+   * so the write-ahead log (WAL) is flushed into the database file.
+   */
+  db.compact();
 }
 
 
