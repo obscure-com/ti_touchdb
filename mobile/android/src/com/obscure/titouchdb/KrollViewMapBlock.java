86c5e2d3800011f3a8e49e811e9a0e755705fb5f
Paul Mietz Egli <paul@obscure.com>
2013-06-06 09:32:40 -0700

    made the emitter static

--- mobile/android/src/com/obscure/titouchdb/KrollViewMapBlock.java
+++ mobile/android/src/com/obscure/titouchdb/KrollViewMapBlock.java
@@ -14,12 +14,12 @@ import com.couchbase.cblite.CBLViewMapEmitBlock;
 @Kroll.proxy(parentModule = TitouchdbModule.class)
 public class KrollViewMapBlock extends KrollProxy implements CBLViewMapBlock {
 
-    private static final String LCAT = "KrollViewMapBlock";
+    private static final String        LCAT = "KrollViewMapBlock";
 
-    private CBLViewMapEmitBlock emitter;
-    
-    private KrollFunction       map;
-    
+    private static CBLViewMapEmitBlock emitter;
+
+    private KrollFunction              map;
+
     public KrollViewMapBlock(KrollFunction map) {
         assert map != null;
         this.map = map;
@@ -29,10 +29,10 @@ public class KrollViewMapBlock extends KrollProxy implements CBLViewMapBlock {
     public void emit(Object key, Object value) {
         emitter.emit(key, value);
     }
-    
+
     @Override
     public void map(Map<String, Object> document, CBLViewMapEmitBlock emitter) {
-        this.emitter = emitter;
+        KrollViewMapBlock.emitter = emitter;
         map.call(this.getKrollObject(), new Object[] { TypePreprocessor.preprocess(document) });
     }
 }
