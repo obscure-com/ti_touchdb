763540f7e2b7e4623ee2b332cfb4950377d150bd
Paul Mietz Egli <paul@obscure.com>
2013-06-06 08:08:34 -0700

    implemented type conversion for maps

--- mobile/android/src/com/obscure/titouchdb/TypePreprocessor.java
+++ mobile/android/src/com/obscure/titouchdb/TypePreprocessor.java
@@ -5,7 +5,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import android.util.Log;
+
 public class TypePreprocessor {
+
+    private static final String LCAT = "TypePreprocessor";
 
     /**
      * Recursively convert List instances to Arrays so they can be passed to
@@ -17,7 +21,7 @@ public class TypePreprocessor {
     @SuppressWarnings({ "unchecked", "rawtypes" })
     public static Object preprocess(Object obj) {
         // short circuit
-        if (!(obj instanceof Collection)) {
+        if (!(obj instanceof Collection || obj instanceof Map)) {
             return obj;
         }
 
@@ -30,14 +34,29 @@ public class TypePreprocessor {
                 }
             }
             catch (InstantiationException e) {
+                Log.e(LCAT, "error creating new collection: " + e.getMessage());
             }
             catch (IllegalAccessException e) {
+                Log.e(LCAT, "error creating new collection: " + e.getMessage());
             }
             return converted.toArray();
         }
         else if (obj instanceof Map) {
-            // TODO convert keys and values
-            return obj;
+            Map converted = null;
+            try {
+                Map map = (Map) obj;
+                converted = (Map) obj.getClass().newInstance();
+                for (Object key : map.keySet()) {
+                    converted.put(preprocess(key), preprocess(map.get(key)));
+                }
+            }
+            catch (InstantiationException e) {
+                Log.e(LCAT, "error creating new map: " + e.getMessage());
+            }
+            catch (IllegalAccessException e) {
+                Log.e(LCAT, "error creating new map: " + e.getMessage());
+            }
+            return converted;
         }
         else {
             return obj;
