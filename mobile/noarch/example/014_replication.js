2305a7d81a4c0101bf8d2fec20aa60c31e3411ff
Paul Mietz Egli <paul@obscure.com>
2013-11-26 14:17:06 -0800

    test for database.allReplications

--- mobile/noarch/example/014_replication.js
+++ mobile/noarch/example/014_replication.js
@@ -26,6 +26,13 @@ exports.run_tests = function() {
       assert(!pull.error, "replication error: "+JSON.stringify(pull.error));
       pullDone = !!(!pull.running && (pull.completed >= pull.total));
     });
+    
+    // check the replication list
+    var repls = db.allReplications;
+    assert(repls, "allReplications returned null");
+    assert(repls.length === 1, "allReplications returned incorrect number of replications: "+repls.length);
+    assert(repls[0].remoteURL === pull.remoteURL, "allReplications remote URL is wrong: "+repls[0].remoteURL);
+    
     pull.start();
     
     // just do pull replication for now
