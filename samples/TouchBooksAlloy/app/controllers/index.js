86440c51f6a3fe5d6e81e65af806663fdff9674c
Paul Mietz Egli <paul@obscure.com>
2014-02-10 08:46:07 -0800

    updated TouchBooksAlloy example to 3.2.0.GA SDK

--- samples/TouchBooksAlloy/app/controllers/index.js
+++ samples/TouchBooksAlloy/app/controllers/index.js
@@ -1,10 +1,3 @@
-if (OS_IOS) {
-  $.main.open();
-}
-else if (OS_ANDROID) {
-  $.main.getView().open();
-}
-
 Ti.App.addEventListener('books:edit_book', function(e) {
   var editBookController = Alloy.createController("edit_book");
   if (e.book_id) {
@@ -12,9 +5,11 @@ Ti.App.addEventListener('books:edit_book', function(e) {
   }
   
   if (OS_IOS) {
-    $.nav.open(editBookController.getView());
+    $.nav.openWindow(editBookController.getView());
   }
   else if (OS_ANDROID) {
     editBookController.getView().open();
   }
 });
+
+$.nav.open();
