4dafa42b85d161148dd7923e81c6620e4d42bf2c
Paul Mietz Egli <paul@obscure.com>
2014-05-30 16:25:27 -0700

    009_views unit tests passing on Android

--- mobile/android/src/com/obscure/titouchdb/KrollReducer.java
+++ mobile/android/src/com/obscure/titouchdb/KrollReducer.java
@@ -2,6 +2,7 @@ package com.obscure.titouchdb;
 
 import java.util.List;
 
+import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.KrollFunction;
 import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.kroll.annotations.Kroll;
@@ -11,7 +12,61 @@ import com.couchbase.lite.Reducer;
 @Kroll.proxy(parentModule = TitouchdbModule.class)
 public class KrollReducer extends KrollProxy implements Reducer {
 
-    private static final String LCAT = "KrollViewReduceBlock";
+    private static final String LCAT = "KrollReducer";
+
+    private static double sum(List<Object> values) {
+        double sum = 0;
+        for (Object value : values) {
+            sum += ((Number) value).doubleValue();
+        }
+        return sum;
+    }
+
+    private static double count(List<Object> values, boolean rereduce) {
+        long count = 0;
+        if (rereduce) {
+            for (Object value : values) {
+                count += ((Number) value).longValue();
+            }
+        }
+        else {
+            return values != null ? values.size() : 0;
+        }
+        return count;
+    }
+    
+    public static final Reducer COUNT = new Reducer() {
+                                          @Override
+                                          public Object reduce(List<Object> keys, List<Object> values, boolean rereduce) {
+                                              return count(values, rereduce);
+                                          }
+                                      };
+
+    public static final Reducer SUM   = new Reducer() {
+                                          @Override
+                                          public Object reduce(List<Object> keys, List<Object> values, boolean rereduce) {
+                                              return sum(values);
+                                          }
+                                      };
+
+    public static final Reducer STATS = new Reducer() {
+                                          @Override
+                                          public Object reduce(List<Object> keys, List<Object> values, boolean rereduce) {
+                                              double min = Double.MAX_VALUE, max = -Double.MAX_VALUE;
+                                              for (Object value : values) {
+                                                  double v = ((Number) value).doubleValue();
+                                                  min = Math.min(min, v);
+                                                  max = Math.max(max, v);
+                                              }
+                                              
+                                              KrollDict result = new KrollDict();
+                                              result.put("sum", sum(values));
+                                              result.put("min", min);
+                                              result.put("max", max);
+                                              result.put("count", count(values, rereduce));
+                                              return result;
+                                          }
+                                      };
 
     private KrollFunction       reduce;
 
--- mobile/android/src/com/obscure/titouchdb/ViewProxy.java
+++ mobile/android/src/com/obscure/titouchdb/ViewProxy.java
@@ -4,43 +4,116 @@ import org.appcelerator.kroll.KrollFunction;
 import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.kroll.annotations.Kroll;
 
+import android.util.Log;
+
+import com.couchbase.lite.Mapper;
+import com.couchbase.lite.Reducer;
 import com.couchbase.lite.View;
 
 @Kroll.proxy(parentModule = TitouchdbModule.class)
 public class ViewProxy extends KrollProxy {
 
-    private View view;
+    private static final String LCAT = "ViewProxy";
 
-    private DatabaseProxy databaseProxy;
-    
+    private DatabaseProxy       databaseProxy;
+
+    private KrollFunction       map;
+
+    private Object              reduce;
+
+    private View                view;
+
     public ViewProxy(DatabaseProxy databaseProxy, View view) {
         assert databaseProxy != null;
         assert view != null;
-        
+
         this.databaseProxy = databaseProxy;
         this.view = view;
     }
-    
+
+    @Kroll.method
+    public QueryProxy createQuery() {
+        return new QueryProxy(databaseProxy, view.createQuery());
+    }
+
+    @Kroll.method
+    public void deleteIndex() {
+        view.deleteIndex();
+    }
+
+    @Kroll.method
+    public void deleteView() {
+        view.delete();
+    }
+
+    @Kroll.getProperty(name = "database")
+    public DatabaseProxy getDatabaseProxy() {
+        return databaseProxy;
+    }
+
+    @Kroll.getProperty(name = "lastSequenceIndexed")
+    public long getLastSequenceIndexed() {
+        return view.getLastSequenceIndexed();
+    }
+
+    @Kroll.getProperty(name = "map")
+    public KrollFunction getMap() {
+        return map;
+    }
+
     @Kroll.getProperty(name = "name")
     public String getName() {
         return view.getName();
     }
 
-    @Kroll.method
-    public boolean setMapAndReduce(KrollFunction map, KrollFunction reduce, String version) {
-        KrollMapper mapblock = new KrollMapper(map);
-        KrollReducer reduceblock = new KrollReducer(reduce);
-        return view.setMapReduce(mapblock, reduceblock, version);
+    @Kroll.getProperty(name = "reduce")
+    public Object getReduce() {
+        return reduce;
+    }
+
+    @Kroll.getProperty(name = "isStale")
+    public boolean isStale() {
+        return view.isStale();
     }
 
     @Kroll.method
     public boolean setMap(KrollFunction map, String version) {
+        this.map = map;
+        this.reduce = null;
+
         KrollMapper mapblock = new KrollMapper(map);
         return view.setMapReduce(mapblock, null, version);
     }
 
     @Kroll.method
-    public QueryProxy query() {
-        return new QueryProxy(databaseProxy, view.createQuery());
+    public boolean setMapReduce(KrollFunction map, Object reduce, String version) {
+        this.map = map;
+        this.reduce = reduce;
+
+        Mapper mapblock = new KrollMapper(map);
+        Reducer reduceblock = null;
+
+        // built-in reduce functions
+        if (reduce instanceof String) {
+            String rname = (String) reduce;
+            if ("_count".equals(rname)) {
+                reduceblock = KrollReducer.COUNT;
+            }
+            else if ("_sum".equals(rname)) {
+                reduceblock = KrollReducer.SUM;
+            }
+            else if ("_stats".equals(rname)) {
+                reduceblock = KrollReducer.STATS;
+            }
+        }
+        else if (reduce instanceof KrollFunction) {
+            reduceblock = new KrollReducer((KrollFunction) reduce);
+        }
+
+        if (reduceblock == null) {
+            Log.e(LCAT, "reduce function not recognized: " + reduce);
+        }
+
+        return view.setMapReduce(mapblock, reduceblock, version);
     }
 }
